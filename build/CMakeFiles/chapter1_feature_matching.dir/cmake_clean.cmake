3c9c065b34749595c0d8f9dd85efc766e40ff19b
hange2017 <914411581@qq.com>
2021-11-15 21:01:12 +0800

    basic env test

---
+++ build/CMakeFiles/chapter1_feature_matching.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter1_feature_matching.dir/chapter1_feature_matching.cpp.o"
+  "chapter1_feature_matching"
+  "chapter1_feature_matching.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chapter1_feature_matching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chapter1_feature_matching.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+
+using namespace std;
+
+int main(int argc,char** argv){
+
+
+
+
+
+    cout<<"chapter1_feature_matching.cpp"<<endl;
+    return 0;
+}
